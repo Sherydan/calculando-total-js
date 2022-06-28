7276d7af4cb4f9638cb9b5972923b93ebdb35fa2
Sherydan <tobar.luis.c@gmail.com>
2022-06-27 22:56:03 -0400

    added add and delete items functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
-function add() {
-    
-}
+let itemQuantity = document.querySelector("#quantity").value;
+let inputQuantity = document.querySelector("#quantity");
+
+let btnAdd = document.querySelector("#btnAdd");
+let btnRemove = document.querySelector("#btnRemove");
+
+let add = function () {
+  if (itemQuantity !== null) {
+    itemQuantity++;
+    inputQuantity.setAttribute("value", itemQuantity);
+    console.log(itemQuantity);
+  }
+};
+
+let remove = function () {
+  if (itemQuantity > 1) {
+    itemQuantity--;
+    inputQuantity.setAttribute("value", itemQuantity);
+    console.log(itemQuantity);
+  }
+};
+
+btnAdd.addEventListener("click", add);
+btnRemove.addEventListener("click", remove);
