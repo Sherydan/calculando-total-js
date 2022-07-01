ed9439ce244b00ad2b61a00ca6a77b05aec5d5ab
Sherydan <tobar.luis.c@gmail.com>
2022-07-01 01:14:39 -0400

    refactorized all my code, renamed main to old-main and vice versa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,18 @@
-// ADD AND REMOVE ITEMS SECTION
-
-// select the current item quantity
-let itemQuantity = document.querySelector("#quantity").value;
-// input to variable
-let inputQuantity = document.querySelector("#quantity");
-
-// buttons to variables
-let btnAdd = document.querySelector("#btnAdd");
-let btnRemove = document.querySelector("#btnRemove");
-
-let add = function () {
-  if (itemQuantity !== null) {
-    // add + 1 to itemQuantity variable
-    itemQuantity++;
-    // change the "value" attribute to input
-    inputQuantity.setAttribute("value", itemQuantity);
-  }
-};
-
-let remove = function () {
-  if (itemQuantity > 1) {
-    itemQuantity--;
-    inputQuantity.setAttribute("value", itemQuantity);
-  }
-};
-
-btnAdd.addEventListener("click", add);
-btnRemove.addEventListener("click", remove);
-// END ADD AND REMOVE ITEMS SECTION
-
-// CALCULATE TOTAL SECTION
-let btnCalcTotal = document.querySelector("#calculateTotal");
+let selectedColor = document.querySelector("#selectedColor");
 let totalPrice = document.querySelector("#totalPrice");
 let unitValue = document.querySelector("#unitValue").innerHTML;
-newUnitValue = unitValue.replace("$", "").replace(".", "");
-
-let formatter = new Intl.NumberFormat("es-CL", {
-  style: "currency",
-  currency: "CLP",
-});
-
-let calcTotal = function () {
-  if (itemQuantity !== null) {
-    totalPrice.innerHTML = formatter.format(parseInt(newUnitValue * itemQuantity));
-  }
-};
-btnCalcTotal.addEventListener("click", calcTotal);
-// END CALCULATE TOTAL SECTION
-
-// SELECT COLOR SECTION
-let btnBlack = document.querySelector("#blackColor");
-let btnRed = document.querySelector("#redColor");
-let btnBlue = document.querySelector("#blueColor");
-let selectedColor = document.querySelector("#selectedColor");
-
-function setColor(color) {
-  selectedColor.innerHTML = color;
-}
-
-btnBlack.addEventListener("click", function(){setColor(btnBlack.getAttribute("colorName"))});
-btnRed.addEventListener("click", function(){setColor(btnRed.getAttribute("colorName"))});
-btnBlue.addEventListener("click", function(){setColor(btnBlue.getAttribute("colorName"))});
-//END SELECT COLOR SECTION
-
+let inputQuantity = document.querySelector("#quantity");
+let totalQuantity = document.querySelector("#totalQuantity");
+let elementsArray = document.querySelectorAll("button");
+elementsArray.forEach(el => el.addEventListener("click", function () {
+    if (el.id === "blackColor" || el.id === "redColor" || el.id === "blueColor" ) {
+        selectedColor.innerHTML = el.attributes[0].value;
+    }
+    if ((el.id === "btnAdd" || el.id === "btnRemove")  ) {
+        inputQuantity.setAttribute("value", (el.attributes[0].value === "-") ? eval(inputQuantity.getAttribute("value") + "+" + "1" ) : (inputQuantity.getAttribute("value") > 1) ? eval(inputQuantity.getAttribute("value") + "-" + "1") : eval(inputQuantity.getAttribute("value") + "-" + "0") )
+    }
+    if (el.id === "calculateTotal") {
+        totalPrice.innerHTML = parseInt(unitValue * inputQuantity.getAttribute("value")).toLocaleString("es-CL", {style: "currency", currency: "CLP"});
+        totalQuantity.innerHTML = inputQuantity.getAttribute("value");
+    }
+}))
