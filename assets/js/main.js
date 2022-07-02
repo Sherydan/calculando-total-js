c0beac817873f7da96fcef96ab928ae0c80376aa
Sherydan <tobar.luis.c@gmail.com>
2022-07-02 00:12:49 -0400

    code refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,13 @@
-let selectedColor = document.querySelector("#selectedColor");
-let totalPrice = document.querySelector("#totalPrice");
-let unitValue = document.querySelector("#unitValue").innerHTML;
 let inputQuantity = document.querySelector("#quantity");
-let totalQuantity = document.querySelector("#totalQuantity");
-let elementsArray = document.querySelectorAll("button");
-elementsArray.forEach(el => el.addEventListener("click", function () {
+document.querySelectorAll("button").forEach(el => el.addEventListener("click", function () {
     if (el.id === "blackColor" || el.id === "redColor" || el.id === "blueColor" ) {
-        selectedColor.innerHTML = el.attributes[0].value;
+        document.querySelector("#selectedColor").innerHTML = el.attributes[0].value;
     }
     if ((el.id === "btnAdd" || el.id === "btnRemove")  ) {
         inputQuantity.setAttribute("value", (el.attributes[0].value === "-") ? eval(inputQuantity.getAttribute("value") + "+" + "1" ) : (inputQuantity.getAttribute("value") > 1) ? eval(inputQuantity.getAttribute("value") + "-" + "1") : eval(inputQuantity.getAttribute("value") + "-" + "0") )
     }
     if (el.id === "calculateTotal") {
-        totalPrice.innerHTML = parseInt(unitValue * inputQuantity.getAttribute("value")).toLocaleString("es-CL", {style: "currency", currency: "CLP"});
-        totalQuantity.innerHTML = inputQuantity.getAttribute("value");
+        document.querySelector("#totalPrice").innerHTML = parseInt(document.querySelector("#unitValue").innerHTML * inputQuantity.getAttribute("value")).toLocaleString("es-CL", {style: "currency", currency: "CLP"});
+        document.querySelector("#totalQuantity").innerHTML = inputQuantity.getAttribute("value");
     }
 }))
