e71a92d3590de9cb8f6b4766723dbcb53510b470
Sherydan <tobar.luis.c@gmail.com>
2022-07-02 22:16:56 -0400

    added color change on clic

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ let inputQuantity = document.querySelector("#quantity");
 document.querySelectorAll("button").forEach(el => el.addEventListener("click", function () {
     if (el.id === "blackColor" || el.id === "redColor" || el.id === "blueColor" ) {
         document.querySelector("#selectedColor").innerHTML = el.attributes[0].value;
+        document.querySelector("#circle").style.color = el.attributes[0].value;
     }
     if ((el.id === "btnAdd" || el.id === "btnRemove")  ) {
         inputQuantity.setAttribute("value", (el.attributes[0].value === "-") ? eval(inputQuantity.getAttribute("value") + "+" + "1" ) : (inputQuantity.getAttribute("value") > 1) ? eval(inputQuantity.getAttribute("value") + "-" + "1") : eval(inputQuantity.getAttribute("value") + "-" + "0") )
